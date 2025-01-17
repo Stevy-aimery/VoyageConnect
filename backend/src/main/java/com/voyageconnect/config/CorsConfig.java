796efbf84f5ee2b784a3b7a19397cc1ea89f45b9
Stevy-aimery <stevyaimery@gmail.com>
2025-01-17 02:24:19 +0100

    Add the connexion with username & pwd

---
+++ backend/src/main/java/com/voyageconnect/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.voyageconnect.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Autorise tous les endpoints
+             // .allowedOrigins("https://abcd1234.ngrok.io", "http://localhost:4200") // Liste des origines autorisées
+                        .allowedOrigins("*") // Autorise toutes les origines
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
+                        .allowedHeaders("*") // Tous les en-têtes autorisés
+                        .allowCredentials(true) // Autorise les cookies ou sessions
+                        .maxAge(3600); // Cache la configuration pendant 1 heure (3600 secondes)
+            }
+        };
+    }
+}
