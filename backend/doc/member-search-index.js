dee9c9b8473e40640cf48a94f0a0673ec697036b
Stevy-aimery <stevyaimery@gmail.com>
2025-01-18 04:40:33 +0100

    Add documentation Swagger API

---
+++ backend/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.voyageconnect.config","c":"SwaggerConfig","l":"customOpenAPI()"},{"p":"com.voyageconnect.config","c":"SwaggerConfig","l":"SwaggerConfig()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ backend/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.voyageconnect.config","l":"SwaggerConfig"}];updateSearchResults();
